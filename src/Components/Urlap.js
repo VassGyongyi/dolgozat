bba04160979a74b5d3ad4cffe7116596f3cd233f
VassGyongyi <s.v.gyongyi@gmail.com>
2024-04-17 20:36:37 +0200

    dolgozat-órai

---
+++ src/Components/Urlap.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import useAdatContext from "../Context/Context";
+import Form from "react-bootstrap/Form";
+
+export default function Urlap() {
+  const { kategoriaLista, setKategoriaLista, tesztLista, setTesztLista } =
+    useAdatContext();
+
+  return (
+    <>
+      <Form.Select aria-label="Default select example">
+        onChange=
+        {(event) => {
+          setKategoriaLista(event.target.value);
+        }}
+        >
+        <option value="valassz">Válassz kategóriát!</option>
+        {kategoriaLista.map((element, index) => {
+          return (
+            <option key={index} value={element}>
+              {element}
+            </option>
+          );
+        })}
+      </Form.Select>
+    </>
+  );
+}
